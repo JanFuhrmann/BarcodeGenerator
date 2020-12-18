5c1bfe9fc269f41c8c84dfd39a2989b450d1bcf8
Jan <st143351@stud.uni-stuttgart.de>
2020-12-18 20:07:27 +0100

    Restructured and commented existing code

--- src/Main.java
+++ src/Main.java
@@ -34,7 +34,7 @@ public class Main {
         } else if (BARCODE.length() == 12) {
             System.out.println("The correct digit for this barcode (" + BARCODE + checkSum + ") is " + checkSum);
         }
-        // visualizeBinCode();
+        visualizeBinCode();
     }
 
     /**
@@ -56,17 +56,82 @@ public class Main {
     }
 
     /**
+     * Calculate the binary code for the known barcode
+     *
+     * @return The binary string
+     */
+    private static String calcBin() {
+        StringBuilder fullCodeText = new StringBuilder("101");
+        boolean drawMiddleLine = true;
+        for (int i = 1; i < barcodeInt.length; i++) {
+            if (i < 7) {
+                if (barcodeInt[i] == 0) {
+                    fullCodeText.append("0001101");
+                } else if (barcodeInt[i] == 1) {
+                    fullCodeText.append("0011001");
+                } else if (barcodeInt[i] == 2) {
+                    fullCodeText.append("0010011");
+                } else if (barcodeInt[i] == 3) {
+                    fullCodeText.append("0111101");
+                } else if (barcodeInt[i] == 4) {
+                    fullCodeText.append("0100011");
+                } else if (barcodeInt[i] == 5) {
+                    fullCodeText.append("0110001");
+                } else if (barcodeInt[i] == 6) {
+                    fullCodeText.append("0101111");
+                } else if (barcodeInt[i] == 7) {
+                    fullCodeText.append("0111011");
+                } else if (barcodeInt[i] == 8) {
+                    fullCodeText.append("0110111");
+                } else if (barcodeInt[i] == 9) {
+                    fullCodeText.append("0001011");
+                }
+            } else {
+                if (drawMiddleLine) {
+                    fullCodeText.append("01010");
+                    drawMiddleLine = false;
+                }
+                if (barcodeInt[i] == 0) {
+                    fullCodeText.append("1110010");
+                } else if (barcodeInt[i] == 1) {
+                    fullCodeText.append("1100110");
+                } else if (barcodeInt[i] == 2) {
+                    fullCodeText.append("1101100");
+                } else if (barcodeInt[i] == 3) {
+                    fullCodeText.append("1000010");
+                } else if (barcodeInt[i] == 4) {
+                    fullCodeText.append("1011100");
+                } else if (barcodeInt[i] == 5) {
+                    fullCodeText.append("1001110");
+                } else if (barcodeInt[i] == 6) {
+                    fullCodeText.append("1010000");
+                } else if (barcodeInt[i] == 7) {
+                    fullCodeText.append("1000100");
+                } else if (barcodeInt[i] == 8) {
+                    fullCodeText.append("1001000");
+                } else if (barcodeInt[i] == 9) {
+                    fullCodeText.append("1110100");
+                }
+            }
+        }
+        fullCodeText.append("101");
+        return fullCodeText.toString();
+    }
+
+    /**
      * This function gets the code via String and formats it for correct output.
      */
     private static void visualizeBinCode() {
-        String visualization = "";
-        for (int i = 0; i < barcodeInt.length; i++) {
-            if (barcodeInt[i] == 0) {
-                visualization += " ";
+        String code = calcBin();
+        StringBuilder visualization = new StringBuilder();
+        for (int i = 0; i < code.length(); i++) {
+            if (code.charAt(i) == '0') {
+                visualization.append(" ");
             } else {
-                visualization += "|";
+                visualization.append("|");
             }
         }
+        System.out.println(code);
         System.out.println(visualization);
     }
 }
