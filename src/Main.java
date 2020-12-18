5b26583d57447024c2eb7fc16a9946c5b3b3ae5f
Jan <st143351@stud.uni-stuttgart.de>
2020-12-18 18:52:39 +0100

    Restructured and commented existing code

--- src/Main.java
+++ src/Main.java
@@ -1,110 +1,72 @@
 public class Main {
+    private static final String BARCODE = "123456789101"; // EAN-13: CC OOOOO AAAAA D with C:country, O:organisation, A:article, D:digit
+    private static int[] barcodeInt;
 
     public static void main(String[] args) {
-	String b = "010101010101";
-	int[] a = speichereArray(b);
-	if (b.length() == 12) {
-	    int pz = berechnePruefziffer(a);
-	    System.out.println("Prüfziffer: " + pz);
-	    a[12] = pz;
-	}
-	String code = berechneCode(a);
-	String vis = visualisiere(code);
-	System.out.println(code);
-	System.out.println(vis);
+        barcodeInt = convertToIntArray();
+        checkDigit();
     }
 
-    private static String visualisiere(String berechneCode) {
-	String vis = "";
-	for (int i = 0; i < berechneCode.length(); i++) {
-	    if (berechneCode.charAt(i) == '0') {
-		vis += " ";
-	    } else {
-		vis += "|";
-	    }
-	}
-	return vis;
+    /**
+     * This function takes the barcode string and extracts every number into an array
+     *
+     * @return The converted Integer array
+     */
+    private static int[] convertToIntArray() {
+        int[] barcodeInt = new int[Main.BARCODE.length() + 1];
+        for (int i = 0; i < Main.BARCODE.length(); i++) {
+            barcodeInt[i] = Integer.parseInt(Main.BARCODE.charAt(i) + "");
+        }
+        return barcodeInt;
     }
 
-    private static String berechneCode(int[] a) {
-	String code = "101";
-	boolean setzeMitte = true;
-	for (int i = 1; i < a.length; i++) {
-	    if (i < 7) {
-		if (a[i] == 0) {
-		    code += "0001101";
-		} else if (a[i] == 1) {
-		    code += "0011001";
-		} else if (a[i] == 2) {
-		    code += "0010011";
-		} else if (a[i] == 3) {
-		    code += "0111101";
-		} else if (a[i] == 4) {
-		    code += "0100011";
-		} else if (a[i] == 5) {
-		    code += "0110001";
-		} else if (a[i] == 6) {
-		    code += "0101111";
-		} else if (a[i] == 7) {
-		    code += "0111011";
-		} else if (a[i] == 8) {
-		    code += "0110111";
-		} else if (a[i] == 9) {
-		    code += "0001011";
-		}
-	    } else {
-		if (setzeMitte) {
-		    code += "01010";
-		    setzeMitte = false;
-		}
-		if (a[i] == 0) {
-		    code += "1110010";
-		} else if (a[i] == 1) {
-		    code += "1100110";
-		} else if (a[i] == 2) {
-		    code += "1101100";
-		} else if (a[i] == 3) {
-		    code += "1000010";
-		} else if (a[i] == 4) {
-		    code += "1011100";
-		} else if (a[i] == 5) {
-		    code += "1001110";
-		} else if (a[i] == 6) {
-		    code += "1010000";
-		} else if (a[i] == 7) {
-		    code += "1000100";
-		} else if (a[i] == 8) {
-		    code += "1001000";
-		} else if (a[i] == 9) {
-		    code += "1110100";
-		}
-	    }
-	}
-	code += "101";
-	return code;
+    /**
+     * Check whether the check digit is correct or (if none existing) calculate one
+     */
+    private static void checkDigit() {
+        int checkSum = calcDigit();
+        if (BARCODE.length() == 13) {
+            if (checkSum == barcodeInt[12]) {
+                System.out.println("This barcode (" + BARCODE + ") is a valid barcode!");
+            } else {
+                System.out.println("This barcode (" + BARCODE + ") is invalid! (Correct digit: " + checkSum + ")");
+            }
+        } else if (BARCODE.length() == 12) {
+            System.out.println("The correct digit for this barcode (" + BARCODE + checkSum + ") is " + checkSum);
+        }
+        // visualizeBinCode();
     }
 
-    private static int[] speichereArray(String b) {
-	int[] a = new int[13];
-	for (int i = 0; i < b.length(); i++) {
-	    a[i] = Integer.parseInt(b.charAt(i) + "");
-	}
-	return a;
+    /**
+     * This function calculates the check digit for the specific barcode
+     *
+     * @return The check digit
+     */
+    private static int calcDigit() {
+        int sum = 0;
+        for (int i = 0; i < 12; i++) {
+            sum += barcodeInt[i]; // if even, add the integer once
+            if (i % 2 == 1) {
+                sum += 2 * barcodeInt[i]; // if uneven, add the integer three times, two times more
+            }
+        }
+        int digit = 10 - sum % 10; // calculate the difference
+        if (digit == 10) digit = 0;
+        return digit;
     }
 
-    private static int berechnePruefziffer(int[] a) {
-	int sum = 0;
-	for (int i = 0; i < a.length; i++) {
-	    if (i % 2 == 0) {
-		sum += a[i];
-	    } else {
-		sum += 3 * a[i];
-	    }
-	}
-	int z = 10 - sum % 10;
-	if (z == 10) {
-	    z = 0;
-	}
-	return z;
+    /**
+     * This function gets the code via String and formats it for correct output.
+     */
+    private static void visualizeBinCode() {
+        String visualization = "";
+        for (int i = 0; i < barcodeInt.length; i++) {
+            if (barcodeInt[i] == 0) {
+                visualization += " ";
+            } else {
+                visualization += "|";
+            }
+        }
+        System.out.println(visualization);
     }
 }
